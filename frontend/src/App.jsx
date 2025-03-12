5658ad372f8c3c1cc038d04de9a1d6140dede3af
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-12 12:12:33 +0530

    created hero section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,19 +14,18 @@ import Navbar from './components/Navbar'
 const App = () => {
     return (
         <div className='px-4 sm:px-[5vw] md:px-[7w] lg:px-[9vw]'>
-            <Navbar>
-                <Routes>
-                    <Route path='/' element={<Home />} />
-                    <Route path='/collection' element={<Collection />} />
-                    <Route path='/about' element={<About />} />
-                    <Route path='/contact' element={<Contact />} />
-                    <Route path='/product/:productId' element={<Product />} />
-                    <Route path='/cart' element={<Cart />} />
-                    <Route path='/login' element={<Login />} />
-                    <Route path='/place-order' element={<PlaceOrder />} />
-                    <Route path='/Orders' element={<Orders />} />
-                </Routes>
-            </Navbar>
+            <Navbar />
+            <Routes>
+                <Route path='/' element={<Home />} />
+                <Route path='/collection' element={<Collection />} />
+                <Route path='/about' element={<About />} />
+                <Route path='/contact' element={<Contact />} />
+                <Route path='/product/:productId' element={<Product />} />
+                <Route path='/cart' element={<Cart />} />
+                <Route path='/login' element={<Login />} />
+                <Route path='/place-order' element={<PlaceOrder />} />
+                <Route path='/Orders' element={<Orders />} />
+            </Routes>
         </div>
     )
 }
---
+++ frontend/src/components/LatestCollection.jsx
@@ -0,0 +1,26 @@
+import React, { useContext, useState } from 'react'
+import { ShopContext } from '../context/ShopContext'
+import Title from './Title'
+
+const LatestCollection = () => {
+    const products = useContext(ShopContext)
+    const [latestProducts, , setLatestProducts,] = useState([])
+
+    // useEffect(() => {
+    //     setLatestProducts(products.slice(0, 10))
+    // }, [])
+
+    return (
+        <div className='my-10'>
+            <div className='text-center py-8 text-3xl'>
+                <Title text1={"LATEST"} text2={"COLLECTIONS"} />
+                <p className='w-3/4 m-auto text-xs sm:text-sm md:text-base text-gray-600'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Autem, temporibus facere.
+                </p>
+            </div>
+        
+        </div>
+    )
+}
+
+export default LatestCollection
