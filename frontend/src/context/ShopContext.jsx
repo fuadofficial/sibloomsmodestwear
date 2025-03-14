619d1765d62338baa24e8316f36b99282c9469b2
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-14 14:44:50 +0530

    completed place order route pge with all functioinalities

--- frontend/src/context/ShopContext.jsx
+++ frontend/src/context/ShopContext.jsx
@@ -1,6 +1,7 @@
 import { createContext, useEffect, useState } from 'react';
 import { products } from '../assets/assets';
 import { toast } from 'react-toastify';
+import { useNavigate } from 'react-router-dom';
 
 export const ShopContext = createContext();
 
@@ -11,6 +12,7 @@ const ShopContextProvider = (props) => {
     const [search, setSearch] = useState("")
     const [showSearch, setShowSearch] = useState(false)
     const [cartItems, setCartItems] = useState({})
+    const navigate = useNavigate()
 
     const addToCart = async (itemId, size) => {
         if (!size) {
@@ -53,7 +55,7 @@ const ShopContextProvider = (props) => {
         setCartItems(cartData)
     }
 
-    const getCartAmount =  () => {
+    const getCartAmount = () => {
         let totalAmount = 0
         for (const items in cartItems) {
             let itemInfo = products.find((product) => product._id === items)
@@ -82,7 +84,8 @@ const ShopContextProvider = (props) => {
         addToCart,
         getCartCount,
         updateQuantity,
-        getCartAmount
+        getCartAmount,
+        navigate
     }
 
     return (
