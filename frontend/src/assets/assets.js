702d4c9417ebd06a38097081e8bf1b5fe602a060
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-16 17:04:10 +0530

    created list product in frontend side

--- frontend/src/assets/assets.js
+++ frontend/src/assets/assets.js
@@ -113,7 +113,7 @@ export const products = [
         name: "Men Round Neck Pure Cotton T-shirt",
         description: "A lightweight, usually knitted, pullover shirt, close-fitting and with a round neckline and short sleeves, worn as an undershirt or outer garment.",
         price: 200,
-        image: [p_img2_1,p_img2_2,p_img2_3,p_img2_4],
+        image: [p_img2_1, p_img2_2, p_img2_3, p_img2_4],
         category: "Men",
         subCategory: "Topwear",
         sizes: ["M", "L", "XL"],
@@ -348,6 +348,10 @@ export const products = [
         date: 1716634345448,
         bestseller: false
     },
+
+
+    // will add these:
+
     {
         _id: "aaaav",
         name: "Women Palazzo Pants with Waist Belt",
