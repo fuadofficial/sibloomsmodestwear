09c2f0f024e29ddd63e961f5f34040b2db97df1b
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-24 09:50:05 +0530

    added some margins and paddings in the footer side

--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -4,7 +4,7 @@ import { assets } from '../assets/assets'
 const Footer = () => {
     return (
         <div>
-            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 my-10 mt-40 text-sm'>
+            <div className='flex flex-col sm:grid grid-cols-[3fr_1fr_1fr] gap-14 mt-21 text-sm'>
                 <div>
                     <img className='mb-5 w-44' src={assets.name} alt="" />
                     <p className='w-full md:w-2/3 text-gray-600'>
