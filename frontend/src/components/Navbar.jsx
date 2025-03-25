08d49539d911164dc5d03242777aa2501f4f2a6e
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-25 13:41:50 +0530

    fix navbar scroll problem

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -72,7 +72,7 @@ const Navbar = () => {
             </div>
 
             {/*Sidebar menu for small screen*/}
-            <div className={`fixed top-0 right-0 bottom-0 bg-white transition-all ${visible ? 'w-full max-h-screen overflow-y-auto' : "w-0 max-h-screen"}`}>
+            <div className={`fixed top-0 right-0 bottom-0 bg-white transition-all ${visible ? 'w-full' : "w-0 max-h-screen"}`}>
             <div className='flex flex-col text-gray-600 '>
                     <div onClick={() => setVisible(false)} className='flex items-center gap-4 p-3 cursor-pointer'>
                         <img className='h-4 rotate-180' src={assets.dropdown_icon} alt="" />
