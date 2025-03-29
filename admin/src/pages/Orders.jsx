71aa766f66b9c6c71f6f74d0083bbab8d268bfb6
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-03-29 15:29:43 +0530

    added some changes for better user frieldly

--- admin/src/pages/Orders.jsx
+++ admin/src/pages/Orders.jsx
@@ -4,7 +4,6 @@ import { useState } from 'react'
 import axios from 'axios'
 import { backendUrl, currency } from '../App'
 import { toast } from 'react-toastify';
-import { assets } from '../assets/assets'
 
 const Orders = ({ token }) => {
 
