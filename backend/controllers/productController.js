15bcd89681ffba0e6b3710edf674197bcab60092
ahmad fuad <ahmadfuadoorakam@gmail.com>
2025-04-05 15:50:01 +0530

    added seo setup in index.html file

--- backend/controllers/productController.js
+++ backend/controllers/productController.js
@@ -52,6 +52,18 @@ const listProducts = async (req, res) => {
     }
 }
 
+// function for edit product 
+// const editProduct = async (req, res) => {
+//     try {
+//         const { productId } = req.body
+//         const product = await productModel.findById(productId)
+//         res.json({ success: true, product })
+//     } catch (error) {
+//         console.log(error);
+//         res.json({ success: false, message: error.message })
+//     }
+// }
+
 // function for remove product
 const removeProduct = async (req, res) => {
     try {
@@ -75,4 +87,4 @@ const singleProduct = async (req, res) => {
     }
 }
 
-export { addProduct, listProducts, removeProduct, singleProduct }
+export { addProduct, listProducts, removeProduct, singleProduct, editProduct }
--- backend/routes/productRoute.js
+++ backend/routes/productRoute.js
@@ -1,5 +1,5 @@
 import express from 'express'
-import { addProduct, listProducts, removeProduct, singleProduct } from "../controllers/productController.js"
+import { addProduct, editProduct, listProducts, removeProduct, singleProduct } from "../controllers/productController.js"
 import upload from '../middleware/multer.js'
 import adminAuth from '../middleware/adminAuth.js'
 
@@ -7,6 +7,7 @@ const porductRouter = express.Router()
 
 porductRouter.post("/add", adminAuth, upload.fields([{ name: "image1", maxCount: 1 }, { name: "image2", maxCount: 1 }, { name: "image3", maxCount: 1 }, { name: "image4", maxCount: 1 }]), addProduct)
 porductRouter.post("/remove", adminAuth, removeProduct)
+// porductRouter.post("/edit", adminAuth, editProduct)
 porductRouter.post("/single", singleProduct)
 porductRouter.get("/list", listProducts)
 
